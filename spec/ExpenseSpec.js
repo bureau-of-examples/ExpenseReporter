89dd1c2ede61ea1b0a6afedef3bc2534c7882d8e
zhy2002 <hz141@uowmail.edu.au>
2015-10-08 20:24:41 +1100

    Your second spec

--- spec/ExpenseSpec.js
+++ spec/ExpenseSpec.js
@@ -1,10 +1,18 @@
 
 describe("Expense objects", function () {
+
+    var expense,expenseItem;
+    beforeEach(function(){
+
+        expenseItem = new ExpenseItem(100);
+        expense = new Expense(expenseItem);
+    });
+
     it("Should set expenseItem property", function () {
-
-        var expenseItem = new ExpenseItem();
-        var expense = new Expense(expenseItem);
-
         expect(expense.expenseItem).toBe(expenseItem);
+    });
+
+    it("Should have the correct expense amount", function(){
+        expect(expense.expenseItem.amount).toEqual(100);
     });
 });
